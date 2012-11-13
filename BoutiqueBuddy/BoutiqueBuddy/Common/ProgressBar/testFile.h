d6b6879c8ff29cc5e945b0af8fbfeeb3928c40cd
vim1486 <vim2k8@gmail.vom>
2012-11-13 11:15:16 +0530

    Adding  DUMMY FILE.

---
+++ BoutiqueBuddy/BoutiqueBuddy/Common/ProgressBar/testFile.h
@@ -0,0 +1,13 @@
+//
+//  testFile.h
+//  BoutiqueBuddy
+//
+//  Created by Vimlesh Bhatt on 11/13/12.
+//  Copyright (c) 2012 Rustedgenius. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testFile : NSObject
+
+@end
---
+++ BoutiqueBuddy/BoutiqueBuddy/Common/ProgressBar/testFile.m
@@ -0,0 +1,13 @@
+//
+//  testFile.m
+//  BoutiqueBuddy
+//
+//  Created by Vimlesh Bhatt on 11/13/12.
+//  Copyright (c) 2012 Rustedgenius. All rights reserved.
+//
+
+#import "testFile.h"
+
+@implementation testFile
+
+@end
